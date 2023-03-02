e2b14c84dd9d3a57990cda93c7e05542ccce5f53
brendon-felix <124619546+brendon-felix@users.noreply.github.com>
2023-03-01 19:10:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,10 @@ from packagename.version import Version
 
 
 setuptools.setup(name='anomaly-detection',
-                 version=Version('1.0.0').number,
+                 version='1.0',
                  description='SynCAN anomaly detection package',
                  long_description=open('README.md').read().strip(),
                  author='Brendon Felix',
                  author_email='brendon.felix.c@gmail.com',
-#                  url='http://path-to-my-packagename',
-                 py_modules=['anomalydetection'],
-#                  install_requires=[],
-#                  license='MIT License',
-#                  zip_safe=False,
-#                  keywords='boilerplate package',
-                 classifiers=['Packages', 'AnomalyDetection'])
+                 packages=['anomaly-detection'],
+                 install_requires=[])
