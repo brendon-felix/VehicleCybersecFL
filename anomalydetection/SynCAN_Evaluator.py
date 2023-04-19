1a67758118cecbc2a41db9765e839011accb0281
brendon-felix <brendon.felix.c@gmail.com>
2023-04-19 20:54:50 +0000

    update

--- anomalydetection/SynCAN_Evaluator.py
+++ anomalydetection/SynCAN_Evaluator.py
@@ -1,3 +1,4 @@
+from functions import *
 class SynCAN_Evaluator:
     '''Class used perform anomaly detection evaluation for INDRA-like models
     '''
@@ -203,11 +204,13 @@ class SynCAN_Evaluator:
         # if self.verbose:
         #     print(f'Percentage of anomalous messages: {np.mean(self.evaluation_df.Label==1)*100:.3f}%')
         results = []
-        for ts in list(thresh_stds):
+        for i, ts in enumerate(list(thresh_stds)):
+            print(f'\rUsing threshold {i+1}/len(list(thresh_stds))', end='')
             if self.verbose:
                 print(f'\nThresholds set to {ts} standard deviations')
             self.set_thresholds(ts, plot=False)
             results.append(self.get_results(reconstructions))
+        print()
         self.results_df = pd.DataFrame(results).set_index(thresh_stds)
         return self.results_df
 
